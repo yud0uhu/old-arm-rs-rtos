d96a34c3a9284b830cbaa1ce92409ecdf3352975
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-11-26 12:44:57 +0900

    feat: QEMUのセットアップ

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
-use cortex_m::asm;
+use core::fmt::Write;
+
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hio};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut stdout = hio::hstdout().unwrap();
+    writeln!(stdout, "Hello, world!").unwrap();
 
-    loop {
-        // your code goes here
-    }
+    // QEMUもしくはデバッガセッションを終了します
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
 }
